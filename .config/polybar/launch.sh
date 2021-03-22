7ff176278c266209d9692786e00e405ce9d9fe20
gcgbarbosa <gcgbarbosa@gmail.com>
2021-03-22 15:46:09 -0700

    Add polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,10 +1,4 @@
-#!/usr/bin/env bash
-
-# set correct resolution
-#xrandr && xrandr --output eDP1 --mode 1920x1080
-
-# load background
-#feh --bg-fill ~/Media/Bilder/Wallpaper/Arch_Arena_Wallpaper_ROT.png
+#!/bin/bash
 
 # Terminate already running bar instances
 killall -q polybar
@@ -12,7 +6,6 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar -r topbar &
+polybar -rq bar &
 
-
+echo "Polybar launched..."
