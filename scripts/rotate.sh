d6d692efc92eeafa621dd56a714f2a896bc95b6b
gcgbarbosa <gcgbarbosa@gmail.com>
2022-04-21 11:06:04 -0700

    Fire

---
+++ scripts/rotate.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# rotates stuff to the right
+xrandr -o right
+# rotate touch screen
+xsetwacom set 'Wacom HID 5115 Finger touch' Rotate cw
+xsetwacom set 'Wacom HID 5115 Finger touch'  MapToOutput eDP-1
+
+
