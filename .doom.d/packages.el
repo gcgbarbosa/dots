e1ef0f19343e8847868847a7cd7ab2dd354e88f8
gcgbarbosa <gcgbarbosa@gmail.com>
2023-03-12 14:49:11 -0700

    Add org-roam-ui (which brings insane value)

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -1,6 +1,10 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
 
+;; org roam UI stuff
+(unpin! org-roam)
+(package! org-roam-ui)
+
 ;; To install a package with Doom you must declare them here and run 'doom sync'
 ;; on the command line, then restart Emacs for the changes to take effect -- or
 ;; use 'M-x doom/reload'.
