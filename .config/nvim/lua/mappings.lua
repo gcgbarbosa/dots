cb75c61dfdb4337ea86c56f0029c347a58cd3b09
gcgbarbosa <gcgbarbosa@gmail.com>
2024-08-07 12:25:47 -0300

    chore: Update dots

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -9,11 +9,17 @@ map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
+-- neogit
 map("n", "<leader>gg", "<cmd> Neogit <cr>", {desc = "Open Neogit"})
-map("n", "<leader>tk", "<cmd> Telescope keymaps <cr>", {desc = "Telescope keymaps"})
 
+-- telescope
+map("n", "<leader>tk", "<cmd> Telescope keymaps <cr>", {desc = "Telescope keymaps"})
 map("n", "<leader><leader>", "<cmd> Telescope command_history <cr>", {desc = "Telescope command history"})
 map("n", ":", "<cmd> Telescope command_history <cr>", {desc = "Telescope command history"})
+
+-- terminal
+map("t", "<C-w>", "<C-\\><C-n>")
+
 
 
 
