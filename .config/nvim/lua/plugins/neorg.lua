f9995b4df3467eb52cbc4ec818173f6b12fc5015
gcgbarbosa <gcgbarbosa@gmail.com>
2024-08-13 21:58:41 -0300

    chore: Move to lazyvim

--- .config/nvim/lua/plugins/neorg.lua
+++ .config/nvim/lua/plugins/neorg.lua
@@ -1,30 +1,28 @@
-local plugins = {
-	"nvim-neorg/neorg",
-	lazy = false,
-	version = "*",
-	config = function()
-		require("neorg").setup({
-			load = {
-				["core.defaults"] = {},
-				["core.concealer"] = {},
-				["core.journal"] = {
-					config = {
-						strategy = "flat",
-					},
-				},
-				["core.dirman"] = {
-					config = {
-						workspaces = {
-							ohack = "~/repos/life/neorg",
-						},
-						default_workspace = "ohack",
-					},
-				},
-			},
-		})
-		vim.wo.foldlevel = 99
-		vim.wo.conceallevel = 2
-	end,
+return {
+  "nvim-neorg/neorg",
+  lazy = false,
+  version = "*",
+  config = function()
+    require("neorg").setup({
+      load = {
+        ["core.defaults"] = {},
+        ["core.concealer"] = {},
+        ["core.journal"] = {
+          config = {
+            strategy = "flat",
+          },
+        },
+        ["core.dirman"] = {
+          config = {
+            workspaces = {
+              ohack = "~/repos/life/neorg",
+            },
+            default_workspace = "ohack",
+          },
+        },
+      },
+    })
+    vim.wo.foldlevel = 99
+    vim.wo.conceallevel = 2
+  end,
 }
-
-return plugins
